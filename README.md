5c2de0acf2d746e026ec61534921f82243822bbd
Georgy Chebanov <georgy@aimfund.com>
2018-02-27 17:25:35 +0300

    add conditional jumps

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 AMD Phenom II X4 965
 ------
-
 alloc 1034581  
 init rnd 404646  
 ticks on data jump: 419  
@@ -9,9 +8,27 @@ ticks on code jump: 497
 
 Macbook pro 2017 i5
 ----------
+alloc 96855  
+init rnd 3072  
+ticks on data jump: 449  
+init rnd 3350  
+ticks on code unconditional jump: 516  
+init rnd 14046  
+ticks on code conditional jump: 540
 
-alloc 148798  
-init rnd 4189  
-ticks on data jump: 447  
-init rnd 8087  
-ticks on code jump: 525
+Intel(R) Core(TM) i7-6950X CPU @ 3.00GHz (overclocked)
+-------
+alloc 23685  
+init rnd 1356  
+ticks on data jump: 283  
+init rnd 1218  
+ticks on code jump: 365
+
+Intel(R) Xeon(R) CPU E5-2697A v4 @ 2.60GHz
+--------
+alloc 26672  
+init rnd 641  
+ticks on data jump: 299  
+init rnd 529  
+ticks on code jump: 347
+
--- main.cpp
+++ main.cpp
@@ -57,12 +57,18 @@ void test0 (uint64_t mem) {
   cout << "ticks on data jump: " << next_t / next_n << endl;
 }
 
-void test1(uint64_t mem) {
+void test1(uint64_t mem, bool conditional = false) {
+  int code0_off = 32;
   unsigned char code0[] = {
+    0x53, 0x51,
     0x31, 0xc0,
-    0xe9, 0x02, 0x00, 0x00, 0x00,
+    0x31, 0xdb,
+    0x31, 0xc9,
     0xff, 0xc0,
-    0xff, 0xc0,
+    0xe9, (unsigned char)(0xff - 5 - 10 - code0_off + 1), 0xff, 0xff, 0xff,
+  };
+  unsigned char code1[] = {
+    0x59, 0x5b,
     0xc3
   };
 #ifdef WIN32
@@ -72,32 +78,63 @@ void test1(uint64_t mem) {
        MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
 #endif
   size_t n = mem / sizeof(M);
-  M* x = (M*)buf;
+  M *x = (M*)buf;
   init_rnd(x, n);
-  M* y = x->prev;
-  for (size_t i = 0; i < n; ++i) {
-    int64_t djump = (char*)x[i].next - (char*)(x + i);
+  M *y = x->prev;
+  uint32_t eax = 1;
+  { size_t i = 0; do {
+    size_t ni = x[i].next - x;
+    int32_t djump = (char*)x[i].next - (char*)(x + i);
     memset(x + i, 0x90, sizeof(M));
-    djump -= 5; // op size
     auto ip = (unsigned char*)(x + i);
-    *ip++ = 0xe9;
-    if ((int32_t)djump != djump) exit(1);
-    *(uint32_t*)(ip) = (int32_t)djump;
-  }
-  memcpy(y, code0, sizeof(code0));
+    if (!conditional) {
+      djump -= 5; // op size
+      *ip++ = 0xe9;
+      if ((int32_t) djump != djump) exit(1);
+      *(int32_t *) (ip) = (int32_t) djump;
+    } else {
+      *ip++ = 0x67;
+      *ip++ = 0x8d;
+      *ip++ = 0x1c;
+      *ip++ = 0x80; //lea
+      *ip++ = 0x89;
+      *ip++ = 0xd8; // mov
+      *ip++ = 0xc1;
+      *ip++ = 0xeb;
+      *ip++ = 0x11; // shr
+      *ip++ = 0x0f;
+      *ip++ = 0x82; //jb
+      eax *= 5;
+      bool cf = (eax >> (0x11 - 1)) & 1;
+      int32_t rjump = rand() % (2 * n) - n;
+      if (!cf) swap(djump, rjump);
+      *(uint32_t *) ip = (uint32_t) (djump + (unsigned char *) (x + i) - ip - 4);
+      ip += 4;
+      *ip++ = 0xe9;
+      *(uint32_t *) ip = (uint32_t) (rjump + (unsigned char *) (x + i) - ip - 4);
+    }
+    i = ni;
+  } while (i != 0); }
+  memcpy((char*)x + code0_off, code0, sizeof(code0));
+  memset(y, 0x90, sizeof(M));
+  memcpy(y, code1, sizeof(code1));
 #ifdef WIN32
   DWORD dummy;
   VirtualProtect(buf, mem, PAGE_EXECUTE_READ, &dummy);
 #endif
-  auto const f = reinterpret_cast<std::int32_t(*)()>(buf);
+  auto const f = reinterpret_cast<std::int32_t(*)()>((char*)buf + code0_off);
   auto next_t = rdtsc();
   size_t next_n = 0;
   for (; next_n < 1; next_n += n) {
     auto const result = f();
-    if (result != 1) exit(1);
+    if (conditional) {
+      if ((5 * result) != eax) exit(1);
+    } else {
+      if (result != 1) exit(1);
+    }
   }
   next_t = rdtsc() - next_t;
-  cout << "ticks on code jump: " << next_t / next_n << endl;
+  cout << "ticks on code " << (conditional ? "" : "un") << "conditional jump: " << next_t / next_n << endl;
 #ifdef WIN32
   VirtualFree(buf, 0, MEM_RELEASE);
 #else
@@ -107,8 +144,10 @@ void test1(uint64_t mem) {
 
 int main() {
   uint64_t mem = (uint64_t(1e9));
+  // mem = 1;
   if (mem & 0xfff) mem = (mem + 0xfff) & ~0xfff;
   test0(mem);
-  test1(mem);
+  test1(mem, false);
+  test1(mem, true);
   return 0;
 }
